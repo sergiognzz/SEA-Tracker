3ee0df629cd00bba632ba800224c4934b05ca00c
Sergio <sergio@localhost.localdomain>
2026-04-29 16:15:16 +0200

    New Functions added and fixed scripts installers (Beta test)

--- unistaller.sh
+++ unistaller.sh
@@ -3,26 +3,9 @@
 set -e
 
 APP_NAME="sea-tracker"
-INSTALL_DIR="/usr/local/bin"
-CONFIG_DIR="$HOME/.config/$APP_NAME"
-SYMLINK_PATH="$INSTALL_DIR/$APP_NAME"
 
 echo "Uninstalling $APP_NAME..."
-
-
-if [ -L "$SYMLINK_PATH" ] || [ -f "$SYMLINK_PATH" ]; then
-    echo "Removing symlink $SYMLINK_PATH"
-    sudo rm -f "$SYMLINK_PATH"
-else
-    echo "Symlink not found at $SYMLINK_PATH"
-fi
-
-
-if [ -d "$CONFIG_DIR" ]; then
-    echo "Removing config directory $CONFIG_DIR"
-    rm -rf "$CONFIG_DIR"
-else
-    echo "Config directory not found at $CONFIG_DIR"
-fi
+sudo rm -rf ~/.config/sea-tracker 2>/dev/null
+sudo rm -rf /usr/local/bin/sea-tracker 2>/dev/null
 
 echo "Uninstallation completed."
